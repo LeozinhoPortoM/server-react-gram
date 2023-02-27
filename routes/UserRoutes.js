1ca507fbbe9cab38f349f78e91057df7cd344352
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-27 12:13:14 -0300

    inserting profile route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login } = require("../controllers/UserController");
+const {
+  register,
+  getCurrentUser,
+  login,
+} = require("../controllers/UserController");
 
 const validate = require("../middlewares/handleValidation");
 const {
   userCreateValidation,
   loginValidation,
 } = require("../middlewares/userValidations");
+const authGuard = require("../middlewares/authGuard");
 
 router.post("/register", userCreateValidation(), validate, register);
+router.get("/profile", authGuard, getCurrentUser);
 router.post("/login", loginValidation(), validate, login);
 
 module.exports = router;
